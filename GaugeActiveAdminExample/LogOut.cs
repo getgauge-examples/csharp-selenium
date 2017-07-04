5e8dec8a1632edd8a6f90c6911f6e9d5b6f5c71b
Gauge <gauge.getgauge.github.com>
2017-07-03 23:52:10 -0700

    c# conventions

---
+++ GaugeActiveAdminExample/LogOut.cs
@@ -0,0 +1,28 @@
+﻿namespace GaugeActiveAdminExample
+{
+    using System;
+    using Gauge.CSharp.Lib.Attribute;
+    using GaugeActiveAdminExample.util;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+
+    public class LogOut
+    {
+
+        [Step("Log out the customer")]
+        public void LogOutTheCustomer()
+        {
+            IWebDriver webDriver = Driver.WebDriver;
+            webDriver.FindElement(By.LinkText("Log out")).Click();
+        }
+
+        [Step("Option <option> should be available")]
+        public void WaitForOptionToBeAvailable(string option)
+        {
+            IWebDriver webDriver = Driver.WebDriver;
+
+            WebDriverWaitWrapper.WaitForElement(By.LinkText(option));
+        }
+
+    }
+}
---
+++ GaugeActiveAdminExample/PlaceOrder.cs
@@ -0,0 +1,37 @@
+﻿namespace GaugeActiveAdminExample
+{
+    using System;
+    using Gauge.CSharp.Lib;
+    using Gauge.CSharp.Lib.Attribute;
+    using GaugeActiveAdminExample.util;
+    using NUnit.Framework;
+    using OpenQA.Selenium;
+    using System.Collections.Generic;
+
+    public class PlaceOrder
+    {
+
+        [Step("Add item <item> to the cart.")]
+        public void AddItemToTheBasket(string item)
+        {
+            IWebDriver webDriver = Driver.WebDriver;
+            webDriver.FindElement(By.LinkText(item)).Click();
+            webDriver.FindElement(By.LinkText("Add to Card")).Click();
+        }
+
+        [Step("Checkout now")]
+        public void PlaceTheOrder()
+        {
+            IWebDriver webDriver = Driver.WebDriver;
+            webDriver.FindElement(By.XPath("//input[@value='Checkout Now!']")).Click();
+        }
+
+        [Step("Cart now contains <itemCount> number of items")]
+        public void CartNowContains(int numberOfItems)
+        {
+            IWebDriver webDriver = Driver.WebDriver;
+            var products = webDriver.FindElements(By.XPath("//table/tbody/tr"));
+            Assert.AreEqual(numberOfItems, products.Count - 2);
+        }
+    }
+}
